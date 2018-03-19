6355e0ccc1ca2511ae9311e88e9fe9663301080d
Gyusun Yeom <omniavinco@gmail.com>
2018-03-19 23:29:25 +0900

    contacts caching

--- src/mail/index.tsx
+++ src/mail/index.tsx
@@ -1,3 +1,6 @@
+import update from "immutability-helper";
+import findIndex from "lodash.findindex";
+import flatten from "lodash.flatten";
 import get from "lodash.get";
 import React from "react";
 import { Grid, Icon, Label, Search, Tab } from "semantic-ui-react";
@@ -36,22 +39,31 @@ interface IStates {
 interface IContact {
     key: string;
     title: string;
-    emails: string[];
+    description: string;
     image: string;
 }
 
 namespace Contact {
-    export function fromPerson(person: gapi.client.people.people.Person): IContact {
-        return {
-            emails: get(person, "emailAddresses", [] as gapi.client.people.people.EmailAddress[])!.map((e) => e.value!),
+    export function fromPerson(person: gapi.client.people.people.Person): IContact[] {
+        return get(
+            person,
+            "emailAddresses",
+            [] as gapi.client.people.people.EmailAddress[],
+        )!.map((email, idx) => ({
+            description: email.value!,
             image: person.photos![0].url!,
-            key: person.resourceName!,
+            key: person.resourceName! + "/" + idx.toString(),
             title: person.names![0].displayName!,
-        };
+        }));
     }
 }
 
 export class MailEditor extends React.Component<{}, IStates> {
+    private static getCacheKey() {
+        const userId = gapi.auth2.getAuthInstance().currentUser.get().getId();
+        return `cached-contacts-${userId}`;
+    }
+
     constructor(props: {}, context: any) {
         super(props, context);
 
@@ -65,11 +77,22 @@ export class MailEditor extends React.Component<{}, IStates> {
 
     public componentDidMount() {
         if (!this.state.contactsInitialized) {
-            this.fetchConnects();
+            const cachedContacts = localStorage.getItem(MailEditor.getCacheKey());
+            if (cachedContacts != null) {
+                const contacts = JSON.parse(cachedContacts) as IContact[];
+                const token = localStorage.getItem(MailEditor.getCacheKey() + "-token")!;
+                updateState(this, {
+                    contacts: {
+                        $set: contacts,
+                    },
+                }).then(() => this.fetchConnects(undefined, token));
+            } else {
+                this.fetchConnects();
+            }
         }
     }
 
-    protected async fetchConnects(token?: string) {
+    protected async fetchConnects(pageToken?: string, syncToken?: string) {
         await updateState(this, {
             contactsFetching: {
                 $set: true,
@@ -79,40 +102,83 @@ export class MailEditor extends React.Component<{}, IStates> {
             },
         });
         const resp = await gapi.client.people.people.connections.list({
-            pageToken: token,
+            pageToken,
             personFields: [
                 "photos", "names", "emailAddresses",
             ].join(","),
+            requestSyncToken: true,
             resourceName: "people/me",
+            syncToken,
         });
+        await updateState(this, {
+            contactsFetching: {
+                $set: false,
+            },
+        });
+
+        if (resp.result.nextSyncToken) {
+            localStorage.setItem(MailEditor.getCacheKey() + "-token", resp.result.nextSyncToken);
+        }
 
         if (resp.result.connections) {
             await updateState(this, {
                 contacts: {
-                    $push: resp.result.connections.map(Contact.fromPerson),
-                },
-                contactsFetching: {
-                    $set: false,
-                },
-                contactsInitialized: {
-                    $set: true,
+                    $push: flatten(resp.result.connections.map(Contact.fromPerson)),
                 },
             });
         }
         if (resp.result.nextPageToken) {
             this.fetchConnects(resp.result.nextPageToken);
+        } else {
+            // last page!
+            // cache contacts
+            localStorage.setItem(
+                MailEditor.getCacheKey(),
+                JSON.stringify(this.state.contacts),
+            );
         }
     }
 
+    private onReceiverSelected(__: any, data: { result: IContact }) {
+        if (findIndex(this.state.receivers, (r) => r.mail === data.result.description) !== -1) {
+            return;
+        }
+        this.setState((state) => update(state, {
+            receivers: {
+                $push: [{
+                    mail: data.result.description,
+                    name: data.result.title,
+                }],
+            },
+        }));
+    }
+
+    private onReceiverRemove(idx: number) {
+        this.setState((state) => update(state, {
+            receivers: {
+                $splice: [idx, 1],
+            },
+        }));
+    }
+
     public render() {
-        return <Grid>
+        return <Grid textAlign="left">
             <Grid.Row>
                 <Grid.Column>
-                    {this.state.receivers.map((receiver) => <Label as="a">
+                    {this.state.receivers.map((receiver, idx) => <Label
+                        as="a" key={`receiver-${idx}`}
+                        onRemove={this.onReceiverRemove.bind(this, idx)}
+                    >
                         {receiver.name}&lt;{receiver.mail}&gt;
                         <Icon name="delete" />
                     </Label>)}
-                    <Search loading={this.state.contactsFetching} results={this.state.contacts} />
+                    <div style={{display: "inline-block"}}>
+                        <Search
+                            loading={this.state.contactsFetching}
+                            results={this.state.contacts}
+                            onResultSelect={this.onReceiverSelected.bind(this)}
+                        />
+                    </div>
                 </Grid.Column>
             </Grid.Row>
             <Grid.Row>
