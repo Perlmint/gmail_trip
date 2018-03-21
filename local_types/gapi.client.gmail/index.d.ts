10e66870d9d8381b43ea4c167c64b427af97099b
Gyusun Yeom <omniavinco@gmail.com>
2018-03-22 03:03:02 +0900

    gmail send types

--- local_types/gapi.client.gmail/index.d.ts
+++ local_types/gapi.client.gmail/index.d.ts
@@ -1,53 +1,75 @@
 
 /// <reference path="../gapi.client/index.d.ts" />
 
-declare namespace gapi.client.gmail.users.messages {
+declare namespace gapi.client.gmail.users {
     type UserId = string | "me";
-    function list(option: ListOption): gapi.client.Request<MessageList>;
-    interface MessageList {
-        messages: Message[];
-        nextPageToken: string;
-        resultSizeEstimate: number;
-    }
-    interface Message {
-        id: string;
-        threadId: string;
-    }
-    interface ListOption {
+    function getProfile(option: GetProfileOption): gapi.client.Request<Profile>;
+    interface GetProfileOption {
         userId: UserId;
-        includeSpamTrash?: boolean;
-        labelIds?: string[];
-        maxResults?: number;
-        pageToken?: string;
-        q?: string;
     }
 
-    function get(option: GetOptionDefault | GetOptionMetadata): gapi.client.Request<MessageDetail>;
-    interface MessageDetail {
-        id: string;
-        threadId: string;
-        labelIds: string[];
-        snippet: string;
-        historyId: string;
-        internalDate: string;
-        payload: {
-            mimeType: string;
-            headers: {
-                name: string;
-                value: string;
-            }[];
-        };
-        sizeEstimate: number;
+    interface Profile {
+        emailAddress: string;
+        messagesTotal: number;
+        threadsTotal: number;
+        historyId: number;
     }
-    interface GetOptionDefault {
-        id: string;
-        userId: UserId;
-        format?: "full" | "minimal" | "raw";
-    }
-    interface GetOptionMetadata {
-        id: string;
-        userId: UserId;
-        format: "metadata";
-        metadataHeaders: string[];
+
+    namespace messages {
+        function list(option: ListOption): gapi.client.Request<MessageList>;
+        interface MessageList {
+            messages: Message[];
+            nextPageToken: string;
+            resultSizeEstimate: number;
+        }
+        interface Message {
+            id: string;
+            threadId: string;
+        }
+        interface ListOption {
+            userId: UserId;
+            includeSpamTrash?: boolean;
+            labelIds?: string[];
+            maxResults?: number;
+            pageToken?: string;
+            q?: string;
+        }
+
+        function get(option: GetOptionDefault | GetOptionMetadata): gapi.client.Request<MessageDetail>;
+        interface MessageDetail {
+            id: string;
+            threadId: string;
+            labelIds: string[];
+            snippet: string;
+            historyId: string;
+            internalDate: string;
+            payload: {
+                mimeType: string;
+                headers: {
+                    name: string;
+                    value: string;
+                }[];
+            };
+            sizeEstimate: number;
+        }
+        interface GetOptionDefault {
+            id: string;
+            userId: UserId;
+            format?: "full" | "minimal" | "raw";
+        }
+        interface GetOptionMetadata {
+            id: string;
+            userId: UserId;
+            format: "metadata";
+            metadataHeaders: string[];
+        }
+
+        function send(option: SendOption): gapi.client.Request<any>;
+        interface SendOption {
+            userId: UserId;
+            resource: {
+                raw: string;
+            };
+        }
     }
 }
